d8aa947b3eb725b52e89bd8834cfd00c5204e06d
shadat-hossan <shadathossan3500@gmail.com>
2024-04-01 16:09:07 +0600

    added location Icon

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const singalCountryMap = document.querySelectorAll(".singalCountryMap");
 
+// singalCountryMap.forEach((i) => {
+//   i.addEventListener("mouseover", function (e) {
+//     console.log(e.target.getAttribute("name"));
+//   });
+// });
+
+singalCountryMap.forEach((i) => {
+    
+  console.log(i.getAttribute("name"));
+});
