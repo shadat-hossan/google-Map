2f1e59ee43afd96e68f7c22ca231e25315e0a126
shadat-hossan <shadathossan3500@gmail.com>
2024-04-02 03:49:21 -0700

    added some style and mark location

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const singalCountryMap = document.querySelectorAll(".singalCountryMap");
 
-// singalCountryMap.forEach((i) => {
-//   i.addEventListener("mouseover", function (e) {
-//     console.log(e.target.getAttribute("name"));
-//   });
-// });
+function positionPinIcon(svgPathSelector, pinIconSelector, topOffset, leftOffset) {
+  const svgPath = document.querySelector(svgPathSelector);
+  const pinIcon = document.querySelector(pinIconSelector);
+  const svgPathRect = svgPath.getBoundingClientRect();
 
-singalCountryMap.forEach((i) => {
-    
-  console.log(i.getAttribute("name"));
-});
+  const relativeTop = svgPathRect.top + window.scrollY + topOffset;
+  const relativeLeft = svgPathRect.left + window.scrollX + leftOffset;
+
+  pinIcon.style.top = relativeTop + "px";
+  pinIcon.style.left = relativeLeft + "px";
+}
+
+function loadAvin() {
+  positionPinIcon(".pniLocationBD", ".singalPinIcon.BD", -22, -3);
+  positionPinIcon(".pniLocationIN", ".singalPinIcon.IN", 30, 22);
+  positionPinIcon(".pniLocationAT", ".singalPinIcon.AT", -28, 1);
+  positionPinIcon(".pniLocationNOR", ".singalPinIcon.NOR", -1, -2);
+}
+
+loadAvin();
+
+window.addEventListener("resize", loadAvin);
+
