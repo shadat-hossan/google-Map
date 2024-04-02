bd630fa447619935aa9328a3d822140b4544b50d
shadat-hossan <shadathossan3500@gmail.com>
2024-04-02 15:12:54 +0600

    added some style and show country name

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const singalCountryMap = document.querySelectorAll(".singalCountryMap");
+const mapNmaeShow = document.querySelector(".mapNmaeShow");
+const mapLayoutImage = document.querySelector(".mapLayoutImage");
 
-function positionPinIcon(svgPathSelector, pinIconSelector, topOffset, leftOffset) {
+function positionPinIcon(
+  svgPathSelector,
+  pinIconSelector,
+  topOffset,
+  leftOffset
+) {
   const svgPath = document.querySelector(svgPathSelector);
   const pinIcon = document.querySelector(pinIconSelector);
   const svgPathRect = svgPath.getBoundingClientRect();
@@ -23,3 +30,44 @@ loadAvin();
 
 window.addEventListener("resize", loadAvin);
 
+async function allCountry() {
+  const API_URL = "https://restcountries.com/v3.1/all";
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  // console.log(data[100].name.common);
+  return data;
+}
+allCountry();
+
+singalCountryMap.forEach(function (i) {
+  i.addEventListener("mousemove", function (e) {
+    let pageY = e.offsetY;
+    let pageX = e.offsetX;
+    mapNmaeShow.style.top = pageY + 10 + "px";
+    mapNmaeShow.style.left = pageX + 10 + "px";
+    mapNmaeShow.style.display = "inline";
+
+    let countryNmae = e.target.getAttribute("name");
+    mapNmaeShow.innerHTML = `<p>${countryNmae}</p>`;
+  });
+});
+
+var mapFluge = false;
+
+mapLayoutImage.addEventListener("click", function (e) {
+  if (mapFluge == false) {
+    document.getElementsByTagName("body")[0].style.background = "#344475";
+    singalCountryMap.forEach(function (element) {
+      element.style.fill = "#2b442d";
+      e.target.src = "img/mapLayout1.png";
+      mapFluge = true;
+    });
+  } else {
+    document.getElementsByTagName("body")[0].style.background = "#6cd2e7";
+    singalCountryMap.forEach(function (element) {
+      element.style.fill = "#fbf8f3";
+      e.target.src = "img/mapLayout2.png";
+      mapFluge = false;
+    });
+  }
+});
